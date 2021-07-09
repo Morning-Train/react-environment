94d08ea7e511149ccfb7b73bbde6ca340b664fdc
Bjarne Bonde <bb@morningtrain.dk>
2021-07-09 11:20:12 +0200

    Added initial documentation setup based on pipelines project

---
+++ documentation/sidebars.js
@@ -0,0 +1,43 @@
+module.exports = {
+  someSidebar: {
+    'Getting started': [
+      'getting_started/introduction',
+      'getting_started/installation',
+      'getting_started/best-practices',
+      'getting_started/examples'
+    ],
+    Pipelines: [
+      'pipelines/pipeline',
+      'pipelines/async-pipeline',
+      'pipelines/nested-pipeline',
+      'pipelines/nested-async-pipeline',
+      'pipelines/conditional-nested-pipeline',
+      'pipelines/conditional-nested-async-pipeline',
+      'pipelines/error-pipeline',
+      'pipelines/error-async-pipeline',
+      'pipelines/on-click-pipeline'
+    ],
+    Pipes: [
+      'pipes/callback-on-pipe'
+    ],
+    Triggers: [
+      'triggers/trigger-pipeline-on-click',
+      'triggers/trigger-pipeline-on-callback'
+    ],
+    Conditionals: [
+      'conditionals/when-env-value-matches',
+    ],
+    Hooks: [
+      'hooks/use-will-pipe',
+      'hooks/use-is-piping',
+      'hooks/use-pipeline'
+    ],
+    Cookbook: [
+      'cookbook/basic-pipeline',
+      'cookbook/basic-async-pipeline',
+      'cookbook/on-click-pipeline',
+      'cookbook/custom-pipes',
+      'cookbook/render-when-piping'
+    ]
+  }
+}
